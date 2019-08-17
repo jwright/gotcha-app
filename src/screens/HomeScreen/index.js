5ec6253bdbca7ed19ace999ff666c6142e316e9a
Jamie Wright <jamie@brilliantfantastic.com>
2019-08-17 10:41:16 -0400

    Turn component into a class component to add button handler

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -10,10 +10,21 @@ const styles = StyleSheet.create({
   },
 });
 
-const HomeScreen = () => (
-  <View style={styles.container}>
-    <Text>You are now viewing the Home screen</Text>
-  </View>
-);
+class HomeScreen extends React.Component {
+  onFindLocationPress() {
+    return alert("Ask for the location");
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to Gotcha.</Text>
+        <Text>We will get you playing in a moment.</Text>
+        <Text>In order to get the arenas you can play in, we need to ask for your location first so you can find one nearby.</Text>
+        <Button onPress={this.onFindLocationPress} title="Find Nearby Arenas" />
+      </View>
+    );
+  }
+}
 
 export default HomeScreen;
