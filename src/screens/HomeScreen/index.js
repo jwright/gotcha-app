730c0b3eb41f4da98a7e2dad7acc1085864ee647
Jamie Wright <jamie@brilliantfantastic.com>
2019-08-17 10:08:03 -0400

    Add rudiementary styles

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 const HomeScreen = () => (
-  <View>
+  <View style={styles.container}>
     <Text>You are now viewing the Home screen</Text>
   </View>
 );
