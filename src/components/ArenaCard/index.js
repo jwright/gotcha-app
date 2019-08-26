99ac5b13ee3505c78f48dbdd6fba8e9f2035014c
Jamie Wright <jamie@brilliantfantastic.com>
2019-08-26 19:24:19 -0400

    Initial display of arena card

--- src/components/ArenaCard/index.js
+++ src/components/ArenaCard/index.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "stretch",
+    justifyContent: "center"
   },
 });
 
-const ArenaCard = ({ arena: { locationName } }) => (
-  <View style={styles.container}>
-    <Text>Display {locationName} Now.</Text>
-  </View>
-);
+const ArenaCard = ({ arena }) => {
+  const { id,
+          locationName,
+          streetAddress1,
+          streetAddress2,
+          city,
+          state,
+          zipCode } = arena;
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={() => alert(`Store ${id}`)}>
+        <Text>{locationName}</Text>
+        <Text>{streetAddress1}</Text>
+        {streetAddress2 && <Text>{streetAddress2}</Text>}
+        <Text>{city}, {state} {zipCode}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default ArenaCard;
