fb64d084a90b777fbd1da484e5672ac527dd9f62
Jamie Wright <jamie@brilliantfantastic.com>
2019-08-26 20:44:18 -0400

    Introduce a provider and default state for the arena provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { ApolloProvider } from "@apollo/react-hooks";
 
 import client from "./helpers/client";
 
+import ArenaContext from "./context/ArenaContext";
+
 import ArenaListScreen from "./screens/ArenaListScreen";
 import HomeScreen from "./screens/HomeScreen";
+import MatchScreen from "./screens/MatchScreen";
 
 const Navigator = createSwitchNavigator(
   {
     ArenaList: { screen: ArenaListScreen },
     Home: { screen: HomeScreen },
+    Match: { screen: MatchScreen },
   },
   {
     initialRouteName: "Home",
@@ -19,10 +23,21 @@ const Navigator = createSwitchNavigator(
 
 const AppNavigationContainer = createAppContainer(Navigator);
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <AppNavigationContainer />
-  </ApolloProvider>
-);
+const App = () => {
+  const [state, setState] = useState({
+    arenaId: null,
+    setArenaId: (arenaId) => {
+      setState({ arenaId });
+    },
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <ArenaContext.Provider value={state}>
+        <AppNavigationContainer />
+      </ArenaContext.Provider>
+    </ApolloProvider>
+  );
+};
 
 export default App;
