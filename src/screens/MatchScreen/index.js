8024b30355c8cdba1b4aac7eae37ceef39eba696
Jamie Wright <jamie@brilliantfantastic.com>
2019-09-02 23:16:43 -0400

    Initial play in arena but wait for a match

--- src/screens/MatchScreen/index.js
+++ src/screens/MatchScreen/index.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useMutation } from "@apollo/react-hooks";
+
+import ArenaContext from "../../context/ArenaContext";
+import PlayArenaMutation from "../../mutations/PlayArena";
+
+import MatchWaiting from "../../components/MatchWaiting";
 
 const styles = StyleSheet.create({
   container: {
@@ -10,10 +16,21 @@ const styles = StyleSheet.create({
   },
 });
 
-const MatchScreen = () => (
-  <View style={styles.container}>
-    <Text>Waiting on a match...</Text>
-  </View>
-);
+const MatchScreen = () => {
+  const [match, setMatch] = useState();
+  const { arenaId } = useContext(ArenaContext);
+  const [playArena] = useMutation(PlayArenaMutation, {
+    onCompleted({ playArena: arena }) {
+      console.log("ARENA FOUND:", arena);
+    },
+  });
+  playArena({ variables: { arenaId: parseInt(arenaId) } });
+
+  return (
+    <View style={styles.container}>
+      { !match && <MatchWaiting /> }
+    </View>
+  );
+};
 
 export default MatchScreen;
