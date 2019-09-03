f8b0004f43e5a9364bfa7d821c7f4163c968b705
Jamie Wright <jamie@brilliantfantastic.com>
2019-09-03 15:08:35 -0400

    Update the match component with the avatar

--- src/components/Match/index.js
+++ src/components/Match/index.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useContext } from "react";
+import { Image, StyleSheet, Text } from "react-native";
+
+import PlayerContext from "../../context/PlayerContext";
+
+const styles = StyleSheet.create({
+  avatar: {
+    borderColor: "#000",
+    borderWidth: 1,
+    height: 100,
+    width: 100,
+  },
+});
 
 const Match = ({ match }) => {
-  const { arena: { locationName }} = match;
+  const { opponent, player } = match;
+  const { player: currentPlayer } = useContext(PlayerContext);
+  const seeker = currentPlayer.id === player.id ? opponent : player;
+  const avatar = seeker.avatar || "https://www.dropbox.com/s/0bhghw81hpw4tqi/avatar.png?raw=1";
 
   return (
     <React.Fragment>
       <Text>You have a new match!</Text>
-      <Text>{locationName}</Text>
+      <Image style={styles.avatar} resizeMode="cover" source={{ uri: avatar }} />
+      <Text>Against: {seeker.name}</Text>
     </React.Fragment>
   );
 };
